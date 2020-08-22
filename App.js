9f705b9bb2714638f7acef79974438a8d245d93c
daniel anggrianto <d.anggrianto@gmail.com>
2020-08-22 00:43:50 -0400

    close #5 : Create Bottom Tab Navigation

--- App.js
+++ App.js
@@ -1,18 +1,27 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
-import { createStackNavigator } from '@react-navigation/stack'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 
 import Home from './screens/Home.js'
+import TabBarIcon from './components/TabBarIcon'
 
-const Stack = createStackNavigator()
+const Tab = createBottomTabNavigator()
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} />
-      </Stack.Navigator>
+      <Tab.Navigator>
+        <Tab.Screen
+          name="Home"
+          component={Home}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <TabBarIcon focused={focused} name="ios-home" />
+            ),
+          }}
+        />
+      </Tab.Navigator>
     </NavigationContainer>
   )
 }
