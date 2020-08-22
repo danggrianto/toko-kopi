786ad64a379dbbf5596f4b024f2d4bd417d56eec
daniel anggrianto <d.anggrianto@gmail.com>
2020-08-21 22:40:50 -0400

    close #1 : Add Firebase

---
+++ config/Firebase.js
@@ -0,0 +1,30 @@
+import * as firebase from 'firebase'
+import '@firebase/auth'
+import '@firebase/firestore'
+import {
+  API_KEY,
+  AUTH_DOMAIN,
+  DATABASE_URL,
+  PROJECT_ID,
+  STORAGE_BUCKET,
+  MESSAGING_SENDER_ID,
+  APP_ID,
+  MEASUREMENT_ID,
+} from '@env'
+
+const firebaseConfig = {
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  databaseURL: DATABASE_URL,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID,
+  measurementId: MEASUREMENT_ID,
+}
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig)
+}
+
+export { firebase }
